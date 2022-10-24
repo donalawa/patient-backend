f7b960be0e547a84f293cec31e5a97de514752e4
donalawa <awadonalcien12@gmail.com>
2022-10-24 06:56:11 +0100

    modified docs

--- Routes/appointment.route.ts
+++ Routes/appointment.route.ts
@@ -16,7 +16,21 @@ const router = express.Router();
  * @api {post} /appointment  Create a new Appointment
  * @apiName Create
  * @apiGroup Appointment
- * @apiBody {String} name       the new name of the item
+ * @apiBody {String} uniqueCode       the unique code of the item 
+ * @apiBody {String} name       the name of the patient
+ * @apiBody {String} gender       the sex of patient
+ * @apiBody {String} phone       the phone of patient
+ * @apiBody {String} email       the email of patient
+ * @apiBody {String} address       the address of patient
+ * @apiBody {String} age       the age of patient
+ * @apiBody {String} city       the city of patient
+ * @apiBody {String} first_time       is it patients first time
+ * @apiBody {String} request_date       patient appointment request date
+ * @apiBody {String} appointment_date       patient  appointment date (optional)
+ * @apiBody {String} appointment_status       patient appointment status
+ * @apiBody {String} appointment_time       patient appointment time (optional)
+ * @apiBody {String} note_before_appointment       note before patients appointment 
+ * @apiBody {String} note_after_appointment       note after patients appointment (optional)
   * @apiPermission secretariat
  * @apiSuccessExample {json} Success-Response:
  *     HTTP/1.1 200 OK
@@ -100,7 +114,21 @@ router.delete("/appointment/:id",  Controllers.deleteAppointment);
  * @apiName Update
  * @apiGroup Appointment
  * @apiParam (Params) {id} id Id of the appointment
- * @apiBody {String} name       the new name of the appointment
+ * @apiBody {String} uniqueCode       the unique code of the item 
+ * @apiBody {String} name       the name of the patient
+ * @apiBody {String} gender       the sex of patient
+ * @apiBody {String} phone       the phone of patient
+ * @apiBody {String} email       the email of patient
+ * @apiBody {String} address       the address of patient
+ * @apiBody {String} age       the age of patient
+ * @apiBody {String} city       the city of patient
+ * @apiBody {String} first_time       is it patients first time
+ * @apiBody {String} request_date       patient appointment request date
+ * @apiBody {String} appointment_date       patient  appointment date (optional)
+ * @apiBody {String} appointment_status       patient appointment status
+ * @apiBody {String} appointment_time       patient appointment time (optional)
+ * @apiBody {String} note_before_appointment       note before patients appointment 
+ * @apiBody {String} note_after_appointment       note after patients appointment (optional)
  * @apiPermission secretariat
  * @apiSuccessExample {json} Success-Response:
  *     HTTP/1.1 200 OK
